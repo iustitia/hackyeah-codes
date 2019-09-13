7a0d28059bbc4fcb4ac532416b6c074a1c1b6a3e
iustitia <jkubisztal@gmail.com>
2019-09-13 10:04:06 +0200

    quiz draft

---
+++ quiz.py
@@ -0,0 +1,12 @@
+questions = ['Jakie miasto jest stolicą Rzymu?',
+             'Ile dni ma lipiec?',
+             'Który z oceanów jest największy?',
+             'Z iloma państwami graniczy Hiszpania?']
+
+answers = ['Rzym', '31', 'Spokojny', '2']
+
+################
+
+"""Stwórz mechanizm quizu dla powyższych pytań i odpowiedzi. 
+Wyświetl pytanie, przyjmij odpowiedź od użytkownika i określ czy jest prawidłowa. Wypisz o tym informację.
+"""
